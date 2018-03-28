1e2d616ea1eb63e26ec3b495ea15ae6be9edb22d
Dominik Menke <dom@digineo.de>
2018-03-29 00:16:48 +0200

    more cleanup

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -2,8 +2,7 @@
 
 SYSPROPS_FILE=${DATADIR}/system.properties
 if [ -f "${SYSPROPS_FILE}" ]; then
-    SYSPROPS_PORT=`grep "^unifi.https.port=" ${SYSPROPS_FILE} | cut -d'=' -f2`
+    SYSPROPS_PORT=$(grep "^unifi.https.port=" ${SYSPROPS_FILE} | cut -d'=' -f2)
 fi
-PORT=${SYSPROPS_PORT:-8443}
 
-curl -kILs --fail https://localhost:${PORT}
+curl -kILs --fail https://localhost:${SYSPROPS_PORT:-8443}
